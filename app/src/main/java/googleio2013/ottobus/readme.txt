ff8ffc2bee8944619c8da20b14231de24b3a59e3
lorence <lorence@enclave.vn>
2018-01-04 16:03:39 +0700

    Understand about Bus

---
+++ app/src/main/java/googleio2013/ottobus/readme.txt
@@ -0,0 +1,10 @@
+> Open github > Open Raw > Paste
+- Change package follow structure
+
+* Ctrl + L:  Directory folder:  /home/lorence/Desktop/Otto.EventBus/app/src/main/java/googleio2013/ottobus
+* Ctrl + L:  Directory layout: /home/lorence/Desktop/Otto.EventBus/app/src/main/res/layout
+* Ctrl + L:  Directory xml: /home/lorence/Desktop/Otto.EventBus/app/src/main/res/xml
+* Ctrl + L:  Directory menu:  /home/lorence/Desktop/Otto.EventBus/app/src/main/res/menu
+* Ctrl + L:  Directory values: /home/lorence/Desktop/Otto.EventBus/app/src/main/res/values
+* Ctrl + L:  Directory drawable: /home/lorence/Desktop/Otto.EventBus/app/src/main/res/drawable
+* Ctrl + L:  Directory mipmap: /home/lorence/Desktop/Otto.EventBus/app/src/main/res/mipmap
---
+++ app/src/main/java/googleio2013/ottobus/singleton/BusProvider.java
@@ -0,0 +1,38 @@
+package googleio2013.ottobus.singleton;/*
+ * Copyright (C) 2012 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import com.squareup.otto.Bus;
+
+/**
+ * Maintains a singleton instance for obtaining the bus. Ideally this would be replaced with a more efficient means
+ * such as through injection directly into interested classes.
+ */
+public final class BusProvider {
+
+  private static Bus mInstance;
+
+  public static Bus getInstance() {
+    if (mInstance == null) {
+      mInstance = new Bus();
+    }
+    return mInstance;
+  }
+
+  private BusProvider() {
+    // No instances.
+  }
+}
